e6e1f68734ae7c93d3aa9db9eae5c812454803cf
Moha-01 <haji.m@web.de>
2021-07-13 11:12:09 +0200

    GUI Start

---
+++ src/GUI/GUI.java
@@ -0,0 +1,26 @@
+package GUI;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class GUI extends JFrame{
+	
+	private JLabel labeltest;
+	
+	
+	public GUI() {
+		setTitle("Lager Spiel");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 400);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import GUI.GUI;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Project starts!");
-    System.out.println("Test!");
+    GUI Spiel = new GUI();
   }
 }
