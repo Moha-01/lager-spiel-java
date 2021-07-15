f6c586ea1c7e06b17eff651a70e0d071fc67a6ed
Moha-01 <haji.m@web.de>
2021-07-15 10:25:23 +0200

    Done GUI

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -38,6 +38,15 @@ public class GUI extends JFrame implements ActionListener{
 	private JButton moveControl = new JButton();
 	private JButton destroyControl = new JButton();
 	private JButton bilanzButton = new JButton();
+	private JButton bNeuerAuftrag = new JButton();
+	private JButton bAuftrag1 = new JButton();
+	private JButton bAuftrag2 = new JButton();
+	private JButton bAuftrag3 = new JButton();
+	private JButton bAuftrag4 = new JButton();
+	private JButton bAblehnen1 = new JButton();
+	private JButton bAblehnen2 = new JButton();
+	private JButton bAblehnen3 = new JButton();
+	private JButton bAblehnen4 = new JButton();
 	
 	/* Underline */
 	private JLabel Label1Regal = new JLabel();
@@ -55,19 +64,47 @@ public class GUI extends JFrame implements ActionListener{
 	private JLabel LabelDestroy = new JLabel();
 	private JLabel lKontostand = new JLabel();
 	private JLabel LabelBilanz = new JLabel();
+	private JLabel lAuftraege = new JLabel();
+	private JLabel colLeft_auf = new JLabel();
+	private JLabel colRight_auf = new JLabel();
+	private JLabel colButtom_auf = new JLabel();
 	
 	private Icon btnIcon = new ImageIcon(GUI.class.getResource("placeholder.png"));
 	private Icon btnDestroy = new ImageIcon(GUI.class.getResource("destroy.png"));
 	private Icon btnMove = new ImageIcon(GUI.class.getResource("move.png"));
 	private Icon btnOut = new ImageIcon(GUI.class.getResource("auslagern.png"));
 	private Icon btnBilanz = new ImageIcon(GUI.class.getResource("bilanz.png"));
+	private Icon btnDelete = new ImageIcon(GUI.class.getResource("ablehnen.png"));
 	
 	private ImageIcon img = new ImageIcon(GUI.class.getResource("gaming.png"));
 	
 	//TextField
 	private JTextField KontostandTextField = new JTextField();
 
-
+	
+	//Variablen
+	//Placeholder
+	int r1v1_ph = 1;
+	int r1v2_ph = 1;
+	int r1v3_ph = 1;
+	int r1v4_ph = 1;
+	int r1v5_ph = 1;
+	int r1h1_ph = 1;
+	int r1h2_ph = 1;
+	int r1h3_ph = 1;
+	int r1h4_ph = 1;
+	int r1h5_ph = 1;
+	int r2v1_ph = 1;
+	int r2v2_ph = 1;
+	int r2v3_ph = 1;
+	int r2v4_ph = 1;
+	int r2v5_ph = 1;
+	int r2h1_ph = 1;
+	int r2h2_ph = 1;
+	int r2h3_ph = 1;
+	int r2h4_ph = 1;
+	int r2h5_ph = 1;
+	
 	public GUI() {
 
 		super();
@@ -75,7 +112,7 @@ public class GUI extends JFrame implements ActionListener{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		
-		int frameWidth = 1600;
+		int frameWidth = 1760;
 	    int frameHeight = 920;
 	    setSize(frameWidth, frameHeight);
 	    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
@@ -86,7 +123,7 @@ public class GUI extends JFrame implements ActionListener{
 		setResizable(true);
 		Container cp = getContentPane();
 	    cp.setLayout(null);
-	    cp.setBackground(Color.LIGHT_GRAY);
+	    cp.setBackground(Color.GRAY);
 	    setIconImage(img.getImage());
 
 
@@ -145,33 +182,26 @@ public class GUI extends JFrame implements ActionListener{
         cp.add(bilanzButton);
         cp.add(LabelBilanz);
         cp.add(KontostandTextField);
+        cp.add(lAuftraege);
+        cp.add(colLeft_auf);
+        cp.add(colRight_auf);
+        cp.add(colButtom_auf);
+        cp.add(bNeuerAuftrag);
+        cp.add(bAuftrag1);
+        cp.add(bAuftrag2);
+        cp.add(bAuftrag3);
+        cp.add(bAuftrag4);
+        cp.add(bAblehnen1);
+        cp.add(bAblehnen2);
+        cp.add(bAblehnen3);
+        cp.add(bAblehnen4);
 
 		setLocationRelativeTo(null);
 		setVisible(true);
 
 	}
-
-	//Placeholder
-	int r1v1_ph = 1;
-	int r1v2_ph = 1;
-	int r1v3_ph = 1;
-	int r1v4_ph = 1;
-	int r1v5_ph = 1;
-	int r1h1_ph = 1;
-	int r1h2_ph = 1;
-	int r1h3_ph = 1;
-	int r1h4_ph = 1;
-	int r1h5_ph = 1;
-	int r2v1_ph = 1;
-	int r2v2_ph = 1;
-	int r2v3_ph = 1;
-	int r2v4_ph = 1;
-	int r2v5_ph = 1;
-	int r2h1_ph = 1;
-	int r2h2_ph = 1;
-	int r2h3_ph = 1;
-	int r2h4_ph = 1;
-	int r2h5_ph = 1;
+	
+	
 	
 	private void initComponents() {
 		
@@ -613,7 +643,7 @@ public class GUI extends JFrame implements ActionListener{
       LabelDestroy.setBackground(Color.RED);
       LabelDestroy.setOpaque(true);
       
-      lKontostand.setBounds(61, 778, 266, 43);
+      lKontostand.setBounds(561, 778, 266, 43);
       lKontostand.setText("  Kontostand:                          €");
       lKontostand.setHorizontalTextPosition(SwingConstants.CENTER);
       lKontostand.setHorizontalAlignment(SwingConstants.LEFT);
@@ -621,7 +651,7 @@ public class GUI extends JFrame implements ActionListener{
       lKontostand.setOpaque(true);
       lKontostand.setFont(new Font("Dialog", Font.BOLD, 18));
       
-      bilanzButton.setBounds(387, 745, 180, 80);
+      bilanzButton.setBounds(887, 745, 180, 80);
       bilanzButton.setIcon(btnBilanz);
       bilanzButton.setBackground(Color.WHITE);
       bilanzButton.setBorder(new javax.swing.border.LineBorder(Color.BLACK));
@@ -633,7 +663,7 @@ public class GUI extends JFrame implements ActionListener{
         }
       });
       
-      LabelBilanz.setBounds(387, 830, 180, 20);
+      LabelBilanz.setBounds(887, 830, 180, 20);
       LabelBilanz.setText("BILANZ");
       LabelBilanz.setHorizontalTextPosition(SwingConstants.CENTER);
       LabelBilanz.setHorizontalAlignment(SwingConstants.CENTER);
@@ -641,10 +671,152 @@ public class GUI extends JFrame implements ActionListener{
       LabelBilanz.setForeground(Color.WHITE);
       LabelBilanz.setOpaque(true);
       
-      KontostandTextField.setBounds(183, 783, 116, 35);
+      KontostandTextField.setBounds(683, 783, 116, 35);
       KontostandTextField.setText("0");
       KontostandTextField.setEditable(false);
       KontostandTextField.setFont(new Font("Dialog", Font.PLAIN, 18));
+      
+      lAuftraege.setBounds(898, 55, 824, 34);
+      lAuftraege.setText("Aufträge");
+      lAuftraege.setHorizontalTextPosition(SwingConstants.CENTER);
+      lAuftraege.setHorizontalAlignment(SwingConstants.CENTER);
+      lAuftraege.setBackground(Color.BLACK);
+      lAuftraege.setOpaque(true);
+      lAuftraege.setForeground(Color.WHITE);
+      
+      colLeft_auf.setBounds(898, 87, 14, 546);
+      colLeft_auf.setText("");
+      colLeft_auf.setBackground(Color.BLACK);
+      colLeft_auf.setOpaque(true);
+      colLeft_auf.setForeground(Color.BLACK);
+      
+      colRight_auf.setBounds(1708, 87, 14, 546);
+      colRight_auf.setText("");
+      colRight_auf.setBackground(Color.BLACK);
+      colRight_auf.setOpaque(true);
+      
+      colButtom_auf.setBounds(898, 616, 823, 17);
+      colButtom_auf.setText("");
+      colButtom_auf.setBackground(Color.BLACK);
+      colButtom_auf.setOpaque(true);
+      
+      bNeuerAuftrag.setBounds(944, 131, 229, 38);
+      bNeuerAuftrag.setText("Neuer Auftrag");
+      bNeuerAuftrag.setBackground(Color.WHITE);
+      bNeuerAuftrag.setBorder(new LineBorder(Color.BLACK));
+      bNeuerAuftrag.setMargin(new Insets(2, 2, 2, 2));
+      bNeuerAuftrag.setFocusPainted(false);
+      bNeuerAuftrag.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      
+      bAuftrag1.setBounds(944, 202, 665, 80);
+      bAuftrag1.setText("Leer");
+      bAuftrag1.setBackground(Color.WHITE);
+      bAuftrag1.setBorder(new LineBorder(Color.BLACK));
+      bAuftrag1.setMargin(new Insets(2, 2, 2, 2));
+      bAuftrag1.setFocusPainted(false);
+      bAuftrag1.setFont(new Font("Dialog", Font.BOLD, 16));
+      bAuftrag1.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      bAuftrag2.setBounds(944, 300, 665, 80);
+      bAuftrag2.setText("Leer");
+      bAuftrag2.setBackground(Color.WHITE);
+      bAuftrag2.setBorder(new LineBorder(Color.BLACK));
+      bAuftrag2.setMargin(new Insets(2, 2, 2, 2));
+      bAuftrag2.setFocusPainted(false);
+      bAuftrag2.setFont(new Font("Dialog", Font.BOLD, 16));
+      bAuftrag2.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      bAuftrag3.setBounds(944, 398, 665, 80);
+      bAuftrag3.setText("Leer");
+      bAuftrag3.setBackground(Color.WHITE);
+      bAuftrag3.setBorder(new LineBorder(Color.BLACK));
+      bAuftrag3.setMargin(new Insets(2, 2, 2, 2));
+      bAuftrag3.setFocusPainted(false);
+      bAuftrag3.setFont(new Font("Dialog", Font.BOLD, 16));
+      bAuftrag3.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      bAuftrag4.setBounds(944, 495, 665, 81);
+      bAuftrag4.setText("Leer");
+      bAuftrag4.setBackground(Color.WHITE);
+      bAuftrag4.setBorder(new LineBorder(Color.BLACK));
+      bAuftrag4.setMargin(new Insets(2, 2, 2, 2));
+      bAuftrag4.setFocusPainted(false);
+      bAuftrag4.setFont(new Font("Dialog", Font.BOLD, 16));
+      bAuftrag4.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      
+      bAblehnen1.setBounds(1616, 202, 75, 80);
+      bAblehnen1.setIcon(btnDelete);
+      bAblehnen1.setBackground(Color.WHITE);
+      bAblehnen1.setBorder(new LineBorder(Color.BLACK));
+      bAblehnen1.setMargin(new Insets(2, 2, 2, 2));
+      bAblehnen1.setFocusPainted(false);
+      bAblehnen1.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      bAblehnen2.setBounds(1616, 300, 75, 80);
+      bAblehnen2.setIcon(btnDelete);
+      bAblehnen2.setBackground(Color.WHITE);
+      bAblehnen2.setBorder(new LineBorder(Color.BLACK));
+      bAblehnen2.setMargin(new Insets(2, 2, 2, 2));
+      bAblehnen2.setFocusPainted(false);
+      bAblehnen2.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      
+      bAblehnen3.setBounds(1616, 398, 75, 80);
+      bAblehnen3.setIcon(btnDelete);
+      bAblehnen3.setBackground(Color.WHITE);
+      bAblehnen3.setBorder(new LineBorder(Color.BLACK));
+      bAblehnen3.setMargin(new Insets(2, 2, 2, 2));
+      bAblehnen3.setFocusPainted(false);
+      bAblehnen3.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      bAblehnen4.setBounds(1616, 495, 75, 81);
+      bAblehnen4.setIcon(btnDelete);
+      bAblehnen4.setBackground(Color.WHITE);
+      bAblehnen4.setBorder(new LineBorder(Color.BLACK));
+      bAblehnen4.setMargin(new Insets(2, 2, 2, 2));
+      bAblehnen4.setFocusPainted(false);
+      bAblehnen4.addActionListener(new ActionListener() { 
+        public void actionPerformed(ActionEvent evt) { 
+        	System.out.println("Klick");
+        }
+      });
+      
+      
+      
       
       
 
